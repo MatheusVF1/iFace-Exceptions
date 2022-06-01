82cde0c02e39ab8c02dd1fd4dc25246d0d7b1ea7
Matheus Vieira <90991829+MatheusVF1@users.noreply.github.com>
2022-05-31 22:37:52 -0300

    Update Community.java

--- iFace/iFace/src/Community.java
+++ iFace/iFace/src/Community.java
@@ -2,8 +2,32 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Community{
-	public String dono, nomeComunidade, descricao;
-	public ArrayList<String> membros = new ArrayList<String>();
+	private String dono, nomeComunidade, descricao;
+	private ArrayList<String> membros = new ArrayList<String>();
+
+    public String getDono(){
+        return dono;
+    }
+
+    public void setDono(String dono){
+        this.dono = dono;
+    }
+
+    public String getNomeComunidade(){
+        return nomeComunidade;
+    }
+
+    public void setNomeComunidade(String nomeComunidade){
+        this.nomeComunidade = nomeComunidade;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
 	
 	public void criarNovaComunidade(String donoComunidade) {	
 		Scanner sc = new Scanner(System.in);
@@ -11,7 +35,7 @@ public class Community{
 		dono = donoComunidade;
 		membros.add(donoComunidade);
 
-		System.out.println("Bem vindo a criação da comunidade! Escreva as informações abaixo.");
+		System.out.println("\nBem vindo a criação da comunidade! Escreva as informações abaixo.");
 
 		System.out.print("Nome da comunidade: ");
 		nomeComunidade = sc.nextLine();
@@ -23,7 +47,23 @@ public class Community{
 		
 	}
 
+    public int encontrarMembro(String pessoa) {
+		for(String val : membros) {
+			if(pessoa.contentEquals(val) == true) {
+				return 1;
+			}
+		}
+		return 0;
+	}
+
+    public void addMembro(String pessoa){
+        membros.add(pessoa);
+    }
 	
+    public void removerMembro(String pessoa){
+        membros.remove(pessoa);
+    }
+
 	public void mostrarComunidade() {
 		System.out.println("\n--------------------------\n" + nomeComunidade.toUpperCase() + "\n--------------------------");
 		System.out.println("Criado por: " + dono);
